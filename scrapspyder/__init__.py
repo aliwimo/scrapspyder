fc3613b2e168a27fcc07f74e44922386a86a65b5
Mohamed Aliwi <aliwimo@gmail.com>
2023-02-25 16:28:44 +0300

    Create `ScrapSpyder` package

---
+++ scrapspyder/__init__.py
@@ -0,0 +1,9 @@
+"""
+ScrapSpyder
+Simple websites scraping package using Python
+"""
+
+from scrapspyder.scraper import Scraper
+from scrapspyder.translator import Translator
+
+__version__ = '0.1.0'
